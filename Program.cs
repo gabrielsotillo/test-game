128dc33bfb41e70885dc220ab296475f9b41279e
Alexander <gsotillo.developer@gmail.com>
2023-06-05 23:16:50 -0400

    menú principal agregado

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string menuOption;
+        
+        while(true){
+
+            menuOption = Console.ReadLine();
+            Console.WriteLine("1. Jugar\n2. Salir");
+            if (menuOption == "1"){
+                break;
+            }
+            else if(menuOption == "2"){
+                Environment.Exit(0);
+            }
+            else{
+                continue;
+            }
+        }
+
+        
+
+
+    }
+}
+
+class Jugador{
+    
+}
